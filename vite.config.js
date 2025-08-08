77f2f5a558b81b698e53871354ac34cb8a2620b5
M A R Emon <maremon15@gmail.com>
2025-08-09 00:29:51 +0600

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-  },
+  base: process.env.VITE_BASE_PATH || "/youtube-clone",
 });
